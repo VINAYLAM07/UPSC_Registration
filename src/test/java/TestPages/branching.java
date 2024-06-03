fd009ec89a231bbd9642f79d414120556703926a
Lam <2303872@cognizant.com>
2024-06-03 16:04:41 +0530

    merging

--- src/test/java/TestPages/branching.java
+++ src/test/java/TestPages/branching.java
@@ -4,6 +4,9 @@ public class branching {
 	
 	public static void  main(String[] args) {
 		System.out.println("Branching successful");
+		
+		System.out.println("Merging Successful into project 1");
+		System.out.println("----------------------------------");
 	}
 
 }
